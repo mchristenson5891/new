fa086b6bbb50449c28346612b05c90ded6ed4668
stephanie lee <stephanielee@stephanies-MacBook-Pro.local>
2020-04-11 10:32:38 -0700

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SignupForm from './components/SignupForm/SignupForm'
 import userService from './utils/userService'
 import { Switch, Route } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
+import LoginPage from './pages/LoginPage/LoginPage'
 
 class App extends React.Component {
   state = {
@@ -19,7 +20,8 @@ class App extends React.Component {
     return (
       <div className="App">
         <NavBar user={this.state.user} handleLogout={this.handleLogout}/>
-        <SignupForm />
+        <SignupForm handleSignupOrLogin={this.handleSignupOrLogin}/>
+        <LoginPage handleSignupOrLogin={this.handleSignupOrLogin} />
       </div>
     );
   }
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import userService from '../../utils/userService'
+
+class LoginPage extends Component {
+  
+  state = {
+    email: '',
+    pw: ''
+  };
+
+  handleChange = (event) => {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      this.props.handleSignupOrLogin();
+    
+    } catch (err) {
+      // Use a modal or toast in your apps instead of alert
+      alert('Invalid Credentials!');
+    }
+  }
+
+  render() {
+    return (
+      <div className="LoginPage">
+        <header className="header-footer">Log In</header>
+        <form className="form-horizontal" onSubmit={this.handleSubmit} >
+          <div className="form-group">
+            <div className="col-sm-12">
+              <input type="email" className="form-control" placeholder="Email" value={this.state.email} name="email" onChange={this.handleChange} />
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-sm-12">
+              <input type="password" className="form-control" placeholder="Password" value={this.state.pw} name="pw" onChange={this.handleChange} />
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-sm-12 text-center">
+              <button className="btn btn-default">Log In</button>&nbsp;&nbsp;&nbsp;
+              <Link to='/'>Cancel</Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
